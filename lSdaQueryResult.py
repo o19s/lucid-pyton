843b3079083d8689ca0372790fccc5bc30390cae
Doug Turnbull <dturnbull@o19s.com>
2013-03-06 10:34:26 -0500

    Add get groups function

--- lSdaQueryResult.py
+++ lSdaQueryResult.py
@@ -5,3 +5,10 @@ def getDocs(queryResult):
         return queryResult['QUERY']['json']['response']['docs']
     except KeyError:
         raise IOError(str(queryResult))
+
+
+def getGroups(queryResult):
+    try:
+        return queryResult['QUERY']['json']['grouped'][1]['groups']
+    except KeyError:
+        raise IOError(str(queryResult))
